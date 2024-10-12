ebb0f9347628f2267c5cfc88bf4ad2a287465640
ryan <ryanroberts562@gmail.com>
2024-10-12 20:38:14 +0000

    basic tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,46 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  console.log('color scheme', colorScheme);
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? 'rocket' : 'rocket-outline'}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="scoreboard"
+        options={{
+          title: 'Scoreboard',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? 'trophy' : 'trophy-outline'}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/scoreboard.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: 35,
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+      <Text>Scoreboard</Text>
+    </View>
+  );
+}
