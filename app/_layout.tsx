a1f23ba0d94de9aeacf5a7d657de6dec157fe9fa
ryan <ryanroberts562@gmail.com>
2024-10-16 09:18:59 +0000

    feat: add google services plist

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,11 @@
 import { Slot } from 'expo-router';
 import { SessionProvider } from '@/providers/SessionProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { configureGoogleSignIn } from '@/services/google-sign-in.service';
 export default function RootLayout() {
   const queryClient = new QueryClient();
+
+  configureGoogleSignIn();
 
   return (
     <QueryClientProvider client={queryClient}>
---
+++ services/google-sign-in.service.ts
@@ -0,0 +1,7 @@
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+function configureGoogleSignIn() {
+  GoogleSignin.configure();
+}
+
+export { configureGoogleSignIn };
